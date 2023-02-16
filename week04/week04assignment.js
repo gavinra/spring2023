1518ec022df6876966be5ec2514fa9a327813073
Gavin Anderson <kjta1015@charter.net>
2023-02-16 18:51:32 -0500

    mostly works

---
+++ week04/week04assignment.js
@@ -0,0 +1,17 @@
+const fs = require('fs')
+console.log("Program Starting")
+fs.readFile(__dirname + '\\assignment.txt', 'utf-8', function(error, data) {
+    if (error) {
+        console.error(error)
+    } else {
+        console.log("Reading a File")
+        var count = 0
+        const lines = data.split("\n")
+        for (var line in lines) {
+            const words = line.split(" ")
+            count = count + words.length
+        }
+    }
+    console.log(`Words: ${count}`)
+    console.log("Done")
+} )
